9db7331809fe13cd134d5e81310b22f069ef8ca0
naduasotelo <sofiasotelo2409@gmail.com>
2025-10-20 21:07:21 +0800

    update app.js

--- app.js
+++ app.js
@@ -1,3 +1,26 @@
-console.log("Header");
-console.log("Body");
-console.log("Footer");
+
+const APP_NAME = "Merge Styles Lab";
+const VERSION = "1.0.0"; 
+
+
+function renderHeader() {
+
+  console.log("Header: Welcome to the Lab");
+}
+
+function renderBody() {
+  console.log("Body: Here we test CI + merge strategies.");
+}
+
+function renderFooter() {
+  console.log("Footer: Thank you for collaborating!");
+}
+
+// Boot
+function main() {
+  renderHeader();
+  renderBody();
+  renderFooter();
+}
+
+main();
